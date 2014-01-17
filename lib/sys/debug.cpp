ea04021aa3eb7d15da6d2ed4bd5239818085439d
David Seal <seal@math.msu.edu>
2014-01-17 15:18:18 -0500

    Removing more references to $DOGPACK.

--- lib/sys/debug.cpp
+++ lib/sys/debug.cpp
@@ -19,7 +19,7 @@ const char *dog_basename (const char *name)
 // === documentation of debug code ===
 //
 // For an example, see the test code:
-//   dogpack/lib/tests/test_debug.cpp
+//   finess/lib/tests/test_debug.cpp
 //
 // eprintf passes its arguments to eprintf_fileLine;
 // derr causes eprintf_fileLine to be called when it goes
@@ -216,7 +216,7 @@ void errmsg_printf_fileLine(const char *func, const char *file, int line_number,
     escape_special_shell_characters(message,escaped_msg);
     str_pos = shell_command;
     str_pos+= sprintf(str_pos, 
-      "${DOGPACK}/scripts/errmsg_printf_script ");
+      "${FINESS}/scripts/errmsg_printf_script ");
     sprintf(str_pos, "%s", escaped_msg);
     
     fprintf(stderr, "executing shell command: %s", shell_command);
