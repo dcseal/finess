213f875d0d07fa6e3636366f2c3c77819df9fb02
David Seal <seal@math.msu.edu>
2014-01-28 17:33:07 -0500

    Adding documentation to the blanks/*.cpp functions

--- lib/1d/blanks/README.md
+++ lib/1d/blanks/README.md
@@ -1,3 +1,8 @@
 # Default (blank) templates
 
-TODO - These "templates" need documentation to be useful.
+This is a collection of potential templates that can be used for each
+application.  It is advisable that the user copies an existing application in
+place of copying or modifying these.
+
+The code assumes that each of these function have been defined in order to
+compile.  For example, please visit the application directory.
--- lib/1d/blanks/SetBndValues.cpp
+++ lib/1d/blanks/SetBndValues.cpp
@@ -1,5 +1,30 @@
 #include "tensors.h"
 
-// Periodic boundary conditions
-void SetBndValues(const dTensor2& node, dTensorBC3& aux, dTensorBC3& q)
-{}
+// *REQUIRED*
+//
+// Boundary conditions specified the the application.
+//
+// Examples include: periodic, zeroth-order extrapolation, 
+//                   reflective (Euler eqns.), ...
+//
+// Inputs:
+//
+//    node( )  - not used in FINESS
+//
+// Outputs:
+//
+//     Modified boundary data for the following arrays:
+//
+//     aux(1-mbc:mx+mbc, 1:maux ) - The auxiliary function
+//       q(1-mbc:mx+mbc, 1:meqn ) - The vector of conserved variables
+//
+// See also: 
+void SetBndValues(const dTensor2& node, dTensorBC2& aux, dTensorBC2& q)
+{
+
+    const int mx     = q.getsize(1);
+    const int meqn   = q.getsize(2);
+    const int maux   = aux.getsize(2);
+    const int mbc    = q.getmbc();
+
+}
