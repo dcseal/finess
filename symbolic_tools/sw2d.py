8f1fa410d538c449afc7481a1a8a9fbf627a824e
David Seal <seal@math.msu.edu>
2014-02-22 14:46:16 -0500

    Shallow water with fixed powers

--- symbolic_tools/sw2d.py
+++ symbolic_tools/sw2d.py
@@ -1,6 +1,10 @@
 from __future__ import print_function  # For printing no newline
-import sympy
- 
+import sympy  # symbolic library
+import re     # regular expressions: for saving useful output
+
+def fixPowers(s):
+    return re.sub(r'q(\d+)\*\*(\d+)', r'pow( q\1, \2 )', s)
+
 meqn = 3
 
 # Conserved variables:
@@ -41,35 +45,39 @@ F = [f1, f2, f3]
 G = [g1, g2, g3]
 
 # A = sympy.Matrix( meqn, meqn )
-print('Computing the Jacobian of the flux function, f')
+print("Computing the Jacobian of the flux function, f'(q)")
 for j in range(meqn):
   for k in range(meqn):
       print( ('Dflux.set(i,%d,%d,1, ' % (j+1,k+1) ), end="" )
-      print( (sympy.simplify( sympy.expand( sympy.diff( F[j], Q[k]))) ), end=");\n")
+      tmp = fixPowers( str(sympy.simplify( sympy.expand( sympy.diff( F[j], Q[k]))) ) )
+      print( tmp, end=");\n")
 print(' ')
 
-print('Computing the Jacobian of the flux function, g')
+print("Computing the Jacobian of the flux function, g'(q)")
 for j in range(meqn):
   for k in range(meqn):
       print( ('Dflux.set(i,%d,%d,2, ' % (j+1,k+1) ), end="" )
-      print( (sympy.simplify( sympy.expand( sympy.diff( G[j], Q[k]))) ), end=");\n")
+      tmp = fixPowers( str(sympy.simplify( sympy.expand( sympy.diff( G[j], Q[k]))) ) )
+      print( tmp, end=");\n")
 print(' ')
 
-print('Computing the Hessian of the flux function, f')
+print("Computing the Hessian of the flux function: f''(q)")
 for m1 in range(meqn):
   print(' ')
   for m2 in range(meqn):
     for m3 in range(meqn):
       print( ('D2flux.set(i,%d,%d,%d,1, ' % (m1+1,m2+1,m3+1) ), end="" )
-      print( (sympy.simplify( sympy.expand( sympy.diff( F[m1], Q[m2], Q[m3]))) ), end=");\n")
+      tmp = fixPowers( str( sympy.expand( sympy.diff( F[m1], Q[m2], Q[m3]))) )
+      print( tmp, end=");\n")
 print(' ')
 
-print('Computing the Hessian of the flux function, g')
+print("Computing the Hessian of the flux function: g''(q)")
 for m1 in range(meqn):
   print(' ')
   for m2 in range(meqn):
     for m3 in range(meqn):
       print( ('D2flux.set(i,%d,%d,%d,2, ' % (m1+1,m2+1,m3+1) ), end="" )
-      print( (sympy.simplify( sympy.expand( sympy.diff( G[m1], Q[m2], Q[m3]))) ), end=");\n")
+      tmp = fixPowers( str( sympy.expand( sympy.diff( G[m1], Q[m2], Q[m3]))) )
+      print( tmp, end=");\n")
 print(' ')
 
