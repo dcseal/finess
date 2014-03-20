2afa1b0485570b992dbe12b7105f62bc8e151bc2
Xiao Feng <matheffex@gmail.com>
2014-03-20 01:16:20 -0400

    Adds WenoReconstruct_JS9().
    Signed-off-by: Xiao Feng <matheffex@gmail.com>

--- lib/WenoReconstruct.cpp
+++ lib/WenoReconstruct.cpp
@@ -163,6 +163,82 @@ static void WenoReconstruct_JS7( const dTensor2& g, dTensor2& diff_g )
 }
 
 
+static void WenoReconstruct_JS9( const dTensor2& g, dTensor2& diff_g )
+{
+    assert_eq(g.getsize(2), 9);
+    double uim4, uim3, uim2, uim1, ui, uip1, uip2, uip3, uip4;
+    double u1, u2, u3, u4, u5;
+    
+    double beta0, beta1, beta2, beta3, beta4;
+    double omt0, omt1, omt2, omt3, omt4, omts;
+
+    const double g0 = 1.0/126.0, g1 = 10./63., g2 = 10./21., g3 = 20./63., g4 = 5./126.;
+
+    const int meqn = g.getsize(1);
+    
+    const double eps = wenoParams.epsilon;  
+    const double power_param = wenoParams.power_param;
+
+
+    for(int m = 1; m <= meqn; m++){
+        uim4 = g.get(m, 1);
+        uim3 = g.get(m, 2);
+        uim2 = g.get(m, 3);
+        uim1 = g.get(m, 4);
+        ui   = g.get(m, 5);
+        uip1 = g.get(m, 6);
+        uip2 = g.get(m, 7);
+        uip3 = g.get(m, 8);
+        uip4 = g.get(m, 9);
+
+        beta0 = uim4*(22658.*uim4 - 208501.*uim3 + 364863.*uim2 - 288007.*uim1 + 86329.*ui) +
+                uim3*(482963.*uim3 - 1704396.*uim2 + 1358458.*uim1 - 411487. * ui) +
+                uim2*(1521393.*uim2 - 2462076.*uim1 + 758823.*ui) + 
+                uim1*(1020563.*uim1 - 649501.*ui) +
+                107918.*pow(ui, 2);
+        beta1 = uim3*(6908.*uim3  - 60871.*uim2 + 99213.*uim1 - 70237.*ui + 18079.*uip1) +
+                uim2*(138563.*uim2 - 464976.*uim1 + 337018.*ui - 88297.*uip1) +
+                uim1*(406293.*uim1 - 611976*ui + 165153.*uip1) +
+                ui*(242723.*ui - 140251.*uip1) + 
+                22658.*pow(uip1, 2);
+        beta2 = uim2*(6908.*uim2 - 51001.*uim1 + 67923.*ui - 38947.*uip1 + 8209.*uip2) +
+                uim1*(104963.*uim1 - 299076.*ui + 179098.*uip1 - 38947.*uip2) +
+                ui*(231153.*ui - 299076.*uip1 + 67923.*uip2) +
+                uip1*(104963.*uip1 - 51001.*uip2) +
+                6908.*pow(uip2, 2);
+        beta3 = uim1*(22658.*uim1 - 140251.*ui + 165153.*uip1 - 88297.*uip2 + 18079.*uip3) +
+                ui*(242723.*ui - 611976.*uip1 + 337018.*uip2 - 70237.*uip3) +
+                uip1*(406293.*uip1 - 464976.*uip2 + 99213.*uip3) +
+                uip2*(138563.*uip2 - 60871.*uip3) +
+                6908.*pow(uip3, 2);
+        beta4 = ui*(107918.*ui - 649501.*uip1 + 758823.*uip2 - 411487.*uip3 + 86329.*uip4) +
+                uip1*(1020563.*uip1 - 2462076.*uip2 + 1358458.*uip3 - 288007*uip4) + 
+                uip2*(1521393.*uip2 - 1704396.*uip3 + 364863.*uip4) +
+                uip3*(482963.*uip3 - 208501.*uip4) +
+                22658.*pow(uip4, 2);
+
+
+        u1 = (1./5.)*uim4   + (-21./20.)*uim3 + (137./60.)*uim2 + (-163./60.)*uim1 + (137./60.)*ui;
+        u2 = (-1./20.)*uim3 + (17./60.)*uim2  + (-43./60.)*uim1 + (77./60.)*ui     + (1./5.)*uip1;
+        u3 = (1./30.)*uim2  + (-13./60.)*uim1 + (47./60.)*ui    + (9./20.)*uip1    + (-1./20.)*uip2;
+        u4 = (-1./20.)*uim1 + (9./20.)*ui     + (47./60.)*uip1  + (-13./60.)*uip2  + (1./30.)*uip3;
+        u5 = (1./5.)*ui     + (77./60.)*uip1  + (-43./60.)*uip2 + (17./60.)*uip3   + (-1./20.)*uip4;
+
+
+        omt0 = g0*pow(eps+beta0, -power_param);
+        omt1 = g1*pow(eps+beta1, -power_param);
+        omt2 = g2*pow(eps+beta2, -power_param);
+        omt3 = g3*pow(eps+beta3, -power_param);
+        omt4 = g4*pow(eps+beta4, -power_param);
+        omts = omt0 + omt1 + omt2 + omt3 + omt4;
+
+        diff_g.set(m, 1, (omt0*u1 + omt1*u2 + omt2*u3 + omt3*u4 + omt4*u5)/omts);
+    }
+}
+
+
+
+
 
 // Central Finite difference approximations:
 //
@@ -222,6 +298,8 @@ void WenoReconstruct(const dTensor2& g, dTensor2& diff_g){
         WenoReconstruct_JS5(g, diff_g);
     else if(wenoParams.weno_version == WENOParams::JS && dogParams.get_space_order() == 7)
         WenoReconstruct_JS7(g, diff_g);
+    else if(wenoParams.weno_version == WENOParams::JS && dogParams.get_space_order() == 9)
+        WenoReconstruct_JS9(g, diff_g);
     else
         throw(std::logic_error("Requested WENO Reconstruction not implemented."));
 }
