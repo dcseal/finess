008b16ca5cb4267e94e10afd11f285b0c7173383
David Seal <seal@math.msu.edu>
2014-07-10 20:34:35 -0400

    adding C0 derivative example to 1d/advection

---
+++ apps/1d/advection/c0deriv/ConSoln.cpp
@@ -0,0 +1,136 @@
+#include <cmath>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include "dogdefs.h"
+#include "DogParams.h"
+#include "DogParamsCart1.h"
+#include "dog_math.h"
+
+using namespace std;
+
+void ConSoln( 
+    const dTensorBC2& aux,
+    const dTensorBC2& q, 
+    double t, string outputdir)
+{
+
+    const int     mx = dogParamsCart1.get_mx();
+    const int   meqn = dogParams.get_meqn();
+    const int   maux = dogParams.get_maux();
+
+    // Grid information:
+    const double dx   = dogParamsCart1.get_dx();
+    const double xlow = dogParamsCart1.get_xlow();
+    const double sqdx = sqrt(dx);
+
+    string fname1 = outputdir+"/conservation.dat";
+    string fname2 = outputdir+"/total-variation.dat";
+
+    ofstream write_file1,write_file2;
+    if( t==0 ) 
+    {
+        write_file1.open(fname1.c_str(), ofstream::out);
+        write_file2.open(fname2.c_str(), ofstream::out);
+    }
+    else
+    {
+        write_file1.open(fname1.c_str(), ofstream::app);
+        write_file2.open(fname2.c_str(), ofstream::app);
+    }
+
+    // -----------------
+    // CONSERVATION
+    // -----------------
+    dTensor2 qsum(meqn, 4); qsum.setall(0.);
+    if( dogParams.get_mcapa() < 1 ) // without capacity function
+    {
+        for (int m=1; m<=meqn; m++)
+        {
+            double maxq = q.get(1,m);
+            for (int i=1; i<=mx; i++)
+            {
+                const double x     = xlow + (double(i)-0.5)*dx;
+                const double qtmp  = q.get(i, m);
+                const double abs_q = fabs( q.get(i, m) );
+
+                qsum.set(m, 1, qsum.get(m,1) +   dx*qtmp       );  // total mass
+                qsum.set(m, 2, qsum.get(m,2) +   dx*abs_q      );  // L1-norm
+                qsum.set(m, 3, qsum.get(m,3) + sqdx*qtmp*qtmp  );  // L2-norm
+                qsum.set(m, 4, Max( abs_q, qsum.get(m,4) )     );  // L-inf norm
+
+            }
+        }
+    }
+    else // with capacity function
+    {
+        for (int m=1; m<=meqn; m++)
+        {
+            for (int i=1; i<=mx; i++)
+            {
+                const double x    = xlow + (double(i)-0.5)*dx;
+                const double qtmp = q.get(i,m);
+                const double atmp = aux.get(i, dogParams.get_mcapa() );
+                const double abs_q = fabs( atmp*q.get(i, m) );
+
+                qsum.set(m, 1, qsum.get(m,1) + atmp*dx*qtmp        );  // total mass
+                qsum.set(m, 2, qsum.get(m,2) + atmp*dx*abs_q       );  // L1-norm
+                qsum.set(m, 3, qsum.get(m,3) + atmp*sqdx*qtmp*qtmp );  // L2-norm
+                qsum.set(m, 4, Max( abs_q, qsum.get(m,4) )         );  // L-inf norm
+            }
+        }
+    }
+
+    // Write data to file
+    write_file1 << setprecision(16);
+    write_file1 << setw(24) << scientific << t << " ";
+    for (int m=1; m<=meqn; m++)
+    {
+        write_file1 << setw(24) << scientific;
+        for( int s=1; s <= 4; s++ )
+        {
+            write_file1 << qsum.get(m,s) << "  ";
+        }
+    }
+    write_file1 << endl;
+
+
+    // Total variation
+    dTensor1 tv(meqn);
+    for (int m=1; m<=meqn; m++)
+    {
+        tv.set(m, fabs(q.get(1,m) - q.get(mx,m) ) );
+        for (int i=2; i<=mx; i++)
+        {
+            tv.set(m, tv.get(m) + fabs( q.get(i,m) - q.get(i-1,m) ) );
+        }
+    }
+
+    // Maximum and minimum value observed during simulation
+    dTensor1 MaxQ(meqn); 
+    dTensor1 MinQ(meqn);
+    for (int m=1; m<=meqn; m++)
+    {
+        MaxQ.set(m, q.get(1,m) );
+        MinQ.set(m, q.get(1,m) );
+        for (int i=2; i<=mx; i++)
+        {
+            MaxQ.set(m, Max( MaxQ.get(m), q.get(i,m) ) );
+            MinQ.set(m, Min( MinQ.get(m), q.get(i,m) ) );
+        }
+    }
+
+
+    write_file2 << setprecision(16);
+    write_file2 << setw(24) << scientific << t << " ";
+    for (int m=1; m<=meqn; m++)
+    {
+        write_file2 << setw(24) << scientific << tv.get(m) << " ";
+        write_file2 << setw(24) << scientific << MinQ.get(m) << " ";
+        write_file2 << setw(24) << scientific << MaxQ.get(m) << " ";
+    }
+    write_file2 << endl;
+
+}
---
+++ apps/1d/advection/c0deriv/SetBndValues.cpp
@@ -0,0 +1,60 @@
+#include "dogdefs.h"
+
+// Periodic boundary conditions
+//
+void SetBndValues(
+    dTensorBC2& aux, 
+    dTensorBC2& q)
+{
+
+    double tmp;
+    const int mx = q.getsize(1);
+    const int meqn   = q.getsize(2);
+    const int maux   = aux.getsize(2);
+    const int mbc    = q.getmbc();
+
+    { 
+        // ***********************************************
+        // LEFT BOUNDARY
+        // ***********************************************
+        for (int i=0; i>=(1-mbc); i--)
+        {        
+	        // q values
+	        for (int m=1; m<=meqn; m++)
+	        {
+	            tmp = q.get(i+mx,m);
+		        q.set(i,m, tmp );
+            }
+                
+	        // aux values
+	        for (int m=1; m<=maux; m++)
+            {
+	            tmp = aux.get(i+mx,m);
+		        aux.set(i,m, tmp );
+            }
+        }
+        // ***********************************************  
+
+
+        // ***********************************************
+        // RIGHT BOUNDARY
+        // ***********************************************
+        for (int i=(mx+1); i<=(mx+mbc); i++)
+        {        
+	        // q values
+	        for (int m=1; m<=meqn; m++)
+	        {
+	            tmp = q.get(i-mx,m);    
+		        q.set(i,m, tmp );
+            }
+                
+	        // aux values
+	        for (int m=1; m<=maux; m++)
+            {
+	            tmp = aux.get(i-mx,m);
+		        aux.set(i,m, tmp );
+            }
+		}
+        // ***********************************************
+    }
+}
