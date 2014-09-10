72ea0f5a4f791da1eb96a5696a20debd485c3963
Xiao Feng <matheffex@gmail.com>
2014-09-10 11:24:18 -0400

    Refactor parsing of an enum to a function.
    - More precisely, a specialization of stringToAny template function.

--- apps/2d/mhd/rotated_shock_tube/Params.cpp
+++ apps/2d/mhd/rotated_shock_tube/Params.cpp
@@ -1,35 +1,45 @@
+/// @file Params.cpp
 #include "Params.h"
 
 Params params;
+
+
+template<>
+Params::ReconstructionMethod::enum_type stringToAny<Params::ReconstructionMethod::enum_type>(const std::string & s){
+    return 
+	s == "A" ? Params::ReconstructionMethod::A :
+	s == "B" ? Params::ReconstructionMethod::B :
+	s == "C" ? Params::ReconstructionMethod::C :
+	Params::ReconstructionMethod::DEFAULT;
+}
+
 
 void Params::init(const std::string& inputFilename){
     using std::string;
     IniDocument ini_doc;
     ini_doc.initFromFile(inputFilename);
 
+
     string reconstruction_method_str = ini_doc["reconstruction"]["method"];
     if(reconstruction_method_str == "")
-	terminate("reconstruction.method is missing.");
-	
-    {
-        this->reconstruction_method =
-	    reconstruction_method_str == "A" ? Params::ReconstructionMethod::A :
-	    reconstruction_method_str == "B" ? Params::ReconstructionMethod::B :
-	    reconstruction_method_str == "C" ? Params::ReconstructionMethod::C :
-	    Params::ReconstructionMethod::DEFAULT;
-    }
+    terminate("reconstruction.method is missing.");
+
+    this->reconstruction_method = stringToAny<Params::ReconstructionMethod::enum_type>(reconstruction_method_str);
 
     if(this->reconstruction_method == Params::ReconstructionMethod::DEFAULT)
-	terminate("reconstruction.method must be one of the following: A, B, C.");
+        terminate("reconstruction.method must be one of the following: A, B, C.");
+    
     string meqn_str = ini_doc["dogParams"]["meqn"];
     if(meqn_str == "")
-	terminate("dogParams.meqn is missing.");
+        terminate("dogParams.meqn is missing.");
 
     {
-	this->meqn = stringToAny<int>(meqn_str);
+        this->meqn = stringToAny<int>(meqn_str);
     }
 
     if(this->meqn <= 0)
-	terminate("dogParams.meqn is expected to be positive.");
-    
+        terminate("dogParams.meqn is expected to be positive.");
+
+
 }
+
