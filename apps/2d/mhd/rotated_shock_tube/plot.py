e709c0b62d7859d86f5184600f3259558b78d1e3
Xiao Feng <matheffex@gmail.com>
2014-09-20 20:49:17 -0400

    Move default plotting code to finess.viz.dim2

--- apps/2d/mhd/rotated_shock_tube/plot.py
+++ apps/2d/mhd/rotated_shock_tube/plot.py
@@ -1,21 +1,5 @@
-
-from finess.params.util import read_params
-from generate_iniparams import parameter_list
-from finess.viz import ask_which_component, interactive_plot
-from mpl_toolkits.mplot3d import Axes3D
-from finess.viz.dim2 import draw_ith_frame_jth_component
+from finess.viz.dim2 \
+  import ask_which_component_and_which_frame_and_plot_wireframe
 
 
-params = read_params("parameters.ini", parameter_list)
-
-component = ask_which_component(params)
-
-draw_ith_frame = \
-    lambda fig, i: \
-       draw_ith_frame_jth_component( \
-         params = params, fig = fig, i = i, j = component,
-         plotting_method_on_Axes3D = Axes3D.plot_wireframe)
-
-interactive_plot(params["finess", "nout"], 
-                 draw_ith_frame = draw_ith_frame)
-
+ask_which_component_and_which_frame_and_plot_wireframe()
--- python/finess/viz/dim2.py
+++ python/finess/viz/dim2.py
@@ -16,5 +16,23 @@ def draw_ith_frame_jth_component(params, fig, i, j,
     X, Y = finess.dim2.meshgrid(params)
     plotting_method_on_Axes3D(ax, X, Y, q[:, :, j - 1])
  
-
-
+def ask_which_component_and_which_frame_and_plot_wireframe():
+    from finess.params.util import read_params
+    from generate_iniparams import parameter_list
+    from finess.viz import ask_which_component, interactive_plot
+    from mpl_toolkits.mplot3d import Axes3D
+    from finess.viz.dim2 import draw_ith_frame_jth_component
+    
+    params = read_params("parameters.ini", parameter_list)
+    
+    component = ask_which_component(params)
+    
+    draw_ith_frame = \
+        lambda fig, i: \
+           draw_ith_frame_jth_component( \
+             params = params, fig = fig, i = i, j = component,
+             plotting_method_on_Axes3D = Axes3D.plot_wireframe)
+    
+    interactive_plot(params["finess", "nout"], 
+                     draw_ith_frame = draw_ith_frame)
+    
