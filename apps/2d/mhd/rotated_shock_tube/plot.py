a3e723031fe772c37ebbb874bd024938b325b751
Xiao Feng <matheffex@gmail.com>
2014-09-20 20:40:21 -0400

    Unify looking of imports.

--- apps/2d/mhd/rotated_shock_tube/plot.py
+++ apps/2d/mhd/rotated_shock_tube/plot.py
@@ -1,17 +1,12 @@
 
-import finess.dim2
-import finess.params.util
-
+from finess.params.util import read_params
 from generate_iniparams import parameter_list
-
-params = finess.params.util.read_params("parameters.ini", parameter_list)
-
 from finess.viz import ask_which_component, interactive_plot
-
 from mpl_toolkits.mplot3d import Axes3D
-
 from finess.viz.dim2 import draw_ith_frame_jth_component
 
+
+params = read_params("parameters.ini", parameter_list)
 
 component = ask_which_component(params)
 
@@ -20,7 +15,6 @@ draw_ith_frame = \
        draw_ith_frame_jth_component( \
          params = params, fig = fig, i = i, j = component,
          plotting_method_on_Axes3D = Axes3D.plot_wireframe)
-
 
 interactive_plot(params["finess", "nout"], 
                  draw_ith_frame = draw_ith_frame)
