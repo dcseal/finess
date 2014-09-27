3a5b77a65f50d2c220fa7c70ad20e94a97edb7de
Xiao Feng <matheffex@gmail.com>
2014-09-27 12:29:07 -0400

    Remove unused RunStartScript() definition.

--- lib/RunStartScript.cpp
+++ lib/RunStartScript.cpp
@@ -28,19 +28,3 @@ void RunStartScript(int ndims )
     int exit_status = system(command_str);
 }
 
-void RunStartScript(int ndims, const char* outputdir)
-{
-    char command_str[1024];
-
-    // run startscript
-    // to create output directory if it does not exist
-    // and copy data files to output directory
-    int numchars = snprintf(command_str,1024,
-            "if test -f startscript && test -x startscript;\n"
-            "then ./startscript %s %d\n"
-            "else ${FINESS}/scripts/startscript %s %d\n"
-            "fi", outputdir, ndims, outputdir, ndims);
-    assert_lt(numchars,1023);
-    assert_gt(numchars,0);
-    int exit_status = system(command_str);
-}
