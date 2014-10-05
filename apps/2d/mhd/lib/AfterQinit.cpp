d8c7d92880bb40bd0b9eee30b56bf1f67a996578
Xiao Feng <matheffex@gmail.com>
2014-10-05 14:38:35 -0400

    Fix 2D MHD AfterQinit signature.

--- apps/2d/mhd/lib/AfterQinit.cpp
+++ apps/2d/mhd/lib/AfterQinit.cpp
@@ -1,10 +1,11 @@
 #include "dogdefs.h"
 #include <string.h>          // For strcpy and strcat (some compilers don't need this)
 #include "IniParams.h"
+#include "StateVars.h"
 
 
 // Function that is called after initial condition
-void AfterQinit(dTensorBC3& aux, dTensorBC3& q)
+void AfterQinit(StateVars& Qstate)
 {
 
   //DO NOTHING
