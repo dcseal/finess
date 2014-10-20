a02cbc898c7a5c96d8357aa9a32cd3a452be8ae2
Xiao Feng <matheffex@gmail.com>
2014-10-20 15:29:02 -0400

    Move updating of A to correct place.
    - So that now A is set at correct time.

--- apps/2d/mhd/rotated_alfven/BeforeStep.cpp
+++ apps/2d/mhd/rotated_alfven/BeforeStep.cpp
@@ -11,45 +11,4 @@
 // Function that is called before each stage
 void BeforeStep(double dt, StateVars& Q)
 {
-    using std::cos;
-    using std::sin;
-    using std::abs;
-    using std::cerr;
-    using std::endl;
-
-    const dTensorBC3& q       = Q.const_ref_q();
-    dTensorBC3& aux     = Q.ref_aux();
-    
-    double t = Q.get_t();
-
-
-    const int mx = global_ini_params.get_mx();
-    const int my = global_ini_params.get_my();
-    const double xlow = global_ini_params.get_xlow();
-    const double ylow = global_ini_params.get_ylow();
-    const double dx = global_ini_params.get_dx();
-    const double dy = global_ini_params.get_dy();
-    const double angle = global_ini_params.get_angle();
-    const int mbc = global_ini_params.get_mbc();
-
-#pragma omp parallel for
-    for(int i = 1 - mbc; i <= mx + mbc; ++i){
-        for(int j = 1 - mbc; j <= my + mbc; ++j){
-//            const double B1 = 
-//                1.0 / (12.0 * dy)
-//                * (aux.get(i, j - 2, 1) - 8*aux.get(i, j-1, 1) 
-//                   + 8*aux.get(i, j+1, 1) - aux.get(i, j+2, 1));
-//            const double B2 =
-//                -1.0 / (12.0 * dx)
-//                * (aux.get(i-2, j, 1) - 8*aux.get(i-1, j, 1)
-//                   + 8*aux.get(i+1, j, 1) - aux.get(i+2, j, 1));
-
-            double x, y;
-            ij_to_xy(xlow, ylow, dx, dy, i, j, x, y);
-            double a3e = A3_exact(angle, t, x, y);
-            if(i < 1 || i > mx || j < 1 || j > my)
-                aux.set(i, j, 1, a3e);
-
-        }
-    } 
  }
