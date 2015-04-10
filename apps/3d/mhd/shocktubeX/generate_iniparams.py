eaa114aff9789f19bd2dad1eb908ddbf54d81f9b
Xiao Feng <matheffex@gmail.com>
2015-04-09 23:43:57 -0400

    Fix parameters problem for some 3D MHD apps.

--- apps/3d/mhd/shocktubeX/generate_iniparams.py
+++ apps/3d/mhd/shocktubeX/generate_iniparams.py
@@ -1,12 +1,17 @@
 from finess.params import append_pac_from_module, write_to_header_cpp
 import finess.params.dim3
 import finess.params.mhd
+import finess.params.artificial_viscosity
+
 import this_app_params
 
 pac = finess.params.dim3.starter_pac()
 
 #section [mhd]
 append_pac_from_module(pac, finess.params.mhd)
+
+#section [av]
+append_pac_from_module(pac, finess.params.artificial_viscosity)
 
 #section [initial]
 append_pac_from_module(pac, this_app_params)
