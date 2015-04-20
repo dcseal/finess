092dbebf8c7f709b60691cfe1f49ff65e794a1fa
David Seal <seal@math.msu.edu>
2015-04-20 01:32:58 -0400

    creating a 2D version of FinSolveUser

--- lib/2d/FinSolveUser.cpp
+++ lib/2d/FinSolveUser.cpp
@@ -4,49 +4,122 @@
 #include "stdlib.h"
 #include "dogdefs.h"
 #include "StateVars.h"
+#include "IniParams.h"
 
 using namespace std;
 
+// ------------------------------------------------------------
+// Function definitions
+void ConSoln( const StateVars& Q );
+
+// Stuff used for a single (Euler) step
+void SetBndValues( StateVars& Q );
+void BeforeStep(double dt, StateVars& Q );
+void ConstructL( 
+        dTensorBC3& aux,
+        dTensorBC3& q,      // setbndy conditions modifies q
+        dTensorBC3& Lstar,
+        dTensorBC2& smax);
+void AfterStep(double dt, StateVars& Q );
+
+double GetCFL(double dt, double dtmax,
+        const dTensorBC3& aux,
+        const dTensorBC3& smax);
+
+void BeforeFullTimeStep (double dt, StateVars& Qold, StateVars& Q);
+void AfterFullTimeStep  (double dt, StateVars& Qold, StateVars& Q);
+// ------------------------------------------------------------
+
+// ------------------------------------------------------------
+// Example function that describes how the main time stepping loop works.
+// 
+// This routine will terminate the code upon entrance.
+// ------------------------------------------------------------
 void FinSolveUser( StateVars& Qnew, double tend, double dtv[] )
 {
 
+    dTensorBC3& qnew = Qnew.ref_q  ();
+    dTensorBC3&  aux = Qnew.ref_aux();
 
-/* TODO - write a template here, and pull it from FinSolveRK.  See the
- * template already created in the 1D code.  (-DS)
+    // Time stepping information
+    const double CFL_max      = global_ini_params.get_max_cfl();      // max CFL number
+    const double CFL_target   = global_ini_params.get_desired_cfl();  // target CFL number
+    double t                  = Qnew.get_t();
+    double dt                 = dtv[1];   // Start with time step from last frame
+    double cfl                = 0.0;      // current CFL number
+    double dtmin              = dt;       // Counters for max and min time step taken
+    double dtmax              = dt;
 
-    while (t<tend)
+    // Declare information about the Runge-Kutta method
+    const int time_order = global_ini_params.get_time_order();
+
+    // Grid information
+    const int mx   = global_ini_params.get_mx();
+    const int my   = global_ini_params.get_my();
+    const int meqn = global_ini_params.get_meqn();
+    const int maux = global_ini_params.get_maux();
+    const int mbc  = global_ini_params.get_mbc();
+    const int numel = qnew.numel();
+
+    // Maximum wave speed at each flux interface value.  Note that the size of
+    // this tensor is one longer in each direction.
+    dTensorBC3 smax( mx+1, my+1, 2, mbc );           
+
+    // Allocate storage for this solver
+    StateVars Qold( t, mx, my, meqn, maux, mbc );
+    dTensorBC3& qold   = Qold.ref_q();
+    dTensorBC3& auxold = Qold.ref_aux();
+    Qold.copyfrom( Qnew );
+
+    // Intermediate stages
+    StateVars Qstar( t, mx, my, meqn, maux, mbc );
+    dTensorBC3&   qstar = Qstar.ref_q();
+    dTensorBC3& auxstar = Qstar.ref_aux();
+    Qstar.copyfrom( Qnew );
+
+    dTensorBC3   Lstar(mx, my, meqn, mbc);   // Right hand side of ODE
+
+    // ---------------------------------------------- //
+    // -- MAIN TIME STEPPING LOOP (for this frame) -- //
+    // ---------------------------------------------- //
+    int n_step   = 0;                           // Number of time steps taken
+    const int nv = global_ini_params.get_nv();  // Maximum allowable time steps
+    while( t<tend )
     {
         // initialize time step
-        m_accept = 0;      
-        n_step = n_step + 1;
+        int m_accept = 0;      
+        n_step       = n_step + 1;
 
         // check if max number of time steps exceeded
-        if (n_step>nv)
+        if( n_step > nv )
         {
-            cout << " Error in DogSolveUser.cpp: "<< 
-                " Exceeded allowed # of time steps " << endl;
-            cout << "    n_step = " << n_step << endl;
-            cout << "        nv = " << nv << endl;
-            cout << endl;
+            printf(" Error in FinSolveRK.cpp: "         );
+            printf("Exceeded allowed # of time steps \n");
+            printf("    n_step = %d\n", n_step          );
+            printf("        nv = %d\n", nv              );
+            printf("Terminating program.\n"             );
             exit(1);
         }        
 
         // copy qnew into qold
-        CopyQ(qnew,qold);
+        Qold.copyfrom( Qnew );
 
         // keep trying until we get time step that doesn't violate CFL condition
-        while (m_accept==0)
+        while( m_accept==0 )
         {
+
             // set current time
-            told = t;
+            Qnew.set_t( t );
+            double told = t;
             if (told+dt > tend)
             { dt = tend - told; }
             t = told + dt;
 
             // Set initial maximum wave speed to zero
-            for (j=1-mbc; j<=(melems+mbc); j++)
-            { smax.set(j, 0.0e0 ); }
+            smax.setall(0.);
 
+            // do any extra work
+            BeforeFullTimeStep(dt, Qold, Qnew);
             // ----------------------------------------------------------------
             //
             //    THIS IS WHERE THE USER-DEFINED TIME-STEPPING SCHEME
@@ -61,17 +134,17 @@ void FinSolveUser( StateVars& Qnew, double tend, double dtv[] )
             exit(1);
             // ----------------------------------------------------------------
 
-            // do any extra work      
-            AfterFullTimeStep(dt,node,prim_vol,auxstar,aux,qold,qnew);
+            // Do any extra work
+            AfterFullTimeStep(dt, Qold, Qnew);
 
             // compute cfl number
-            cfl = GetCFL(dt,dtv[2],prim_vol,method,aux,smax);
+            cfl = GetCFL(dt, dtv[2], aux, smax);
 
             // output time step information
-            if (method[4]>0) 
+            if( global_ini_params.get_verbosity() )
             {
                 cout << setprecision(3);
-                cout << "DogSolve1D ... Step" << setw(5) << n_step;
+                cout << "FinSolve2D ... Step" << setw(5) << n_step;
                 cout << "   CFL =" << setw(6) << fixed << cfl;
                 cout << "   dt =" << setw(11) << scientific << dt;
                 cout << "   t =" << setw(11) << scientific << t <<endl;
@@ -90,34 +163,30 @@ void FinSolveUser( StateVars& Qnew, double tend, double dtv[] )
             }
 
             // see whether to accept or reject this step
-            if (cfl<=CFL_max)
-                // accept
+            if (cfl<=CFL_max)       // accept
             { m_accept = 1; }
-            else 
-                //reject
+            else                    //reject
             {   
                 t = told;
-                if (method[4]>0)
+                if( global_ini_params.get_verbosity() )
                 {
-                    cout<<"DogSolve1D rejecting step...";
+                    cout<<"FinSolve2D rejecting step...";
                     cout<<"CFL number too large";
                     cout<<endl;
                 }
 
                 // copy qold into qnew
-                CopyQ(qold,qnew);
+                Qnew.copyfrom( Qold );
             }
 
-        }
+        } // End of m_accept loop
 
         // compute conservation and print to file
-        ConSoln(method,node,aux,qnew,t,outputdir);
+        ConSoln( Qnew );
 
-    }
+    } // End of while loop
 
-    // set initial time step for next call to DogSolve
+    // set initial time step for next call to DogSolve:
     dtv[1] = dt;
-
-*/
 
 }
