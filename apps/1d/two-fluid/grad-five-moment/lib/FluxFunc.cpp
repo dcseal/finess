e0f601c47d5f52c2459d7f1e8452976f5fdcf9f5
David Seal <seal@math.msu.edu>
2015-08-26 00:37:05 -0400

    redoing fluxfunc and sourctermfunc

--- apps/1d/two-fluid/grad-five-moment/lib/FluxFunc.cpp
+++ apps/1d/two-fluid/grad-five-moment/lib/FluxFunc.cpp
@@ -18,19 +18,87 @@ void FluxFunc(const dTensor1& xpts, const dTensor2& Q, const dTensor2& Aux, dTen
 
     // First two blocks of flux function look like Euler (one for ions, and
     // one for electrons)
-    void FiveMomentFluxFunc( int n_offset, const dTensor2& Q, dTensor2& flux);
-    FiveMomentFluxFunc(0, Q, flux);     // Components for ions
-    FiveMomentFluxFunc(5, Q, flux);     // Components for electrons
+//  void FiveMomentFluxFunc( int n_offset, const dTensor2& Q, dTensor2& flux);
+//  FiveMomentFluxFunc(0, Q, flux);     // Components for ions
+//  FiveMomentFluxFunc(5, Q, flux);     // Components for electrons
 
-    // Last components describe Maxwell's equations
-    void MaxwellFluxFunc( int n_offset, const dTensor2& Q, dTensor2& flux);
-    MaxwellFluxFunc(10, Q, flux);
+//  // Last components describe Maxwell's equations
+//  void MaxwellFluxFunc( int n_offset, const dTensor2& Q, dTensor2& flux);
+//  MaxwellFluxFunc(10, Q, flux);
 
     // TODO - I'm not sure what this part is about ... -DS
 //  void AdvectionFluxFunc( const dTensor2& Q, dTensor3& flux, int advIdx, int rhoIdx);
 //  if(Q.getsize(2)<_entropy_i) return;
 //  AdvectionFluxFunc(Q, flux, _entropy_i, _rho_i);
 //  AdvectionFluxFunc(Q, flux, _entropy_e, _rho_e);
+
+    // ---- This was copied directly from DoGPack ---- //
+    // TODO - reuse FiveMomentFluxFunc ?? -DS
+
+    const int numpts=xpts.getsize();
+    double gamma,mass_ratio,debye,cs_light,larmor_radius;
+    double rho_i,u1_i,u2_i,u3_i,press_i,energy_i;
+    double rho_e,u1_e,u2_e,u3_e,press_e,energy_e;
+    double B1,B2,B3,E1,E2,E3;
+
+    for (int i=1; i<=numpts; i++)
+    {
+        const double x = xpts.get(i);
+
+        // Parameters
+        const double mass_ratio     = global_ini_params.get_mass_ratio  ();
+        const double debye          = global_ini_params.get_debye_length();
+        const double larmor_radius  = global_ini_params.get_larmor_radius();
+        const double gamma          = global_ini_params.get_gamma();
+        const double cs_light       = global_ini_params.get_cs_light();
+
+        // Variables
+        rho_i    = Q.get(i,1);
+        u1_i     = Q.get(i,2)/rho_i;
+        u2_i     = Q.get(i,3)/rho_i;
+        u3_i     = Q.get(i,4)/rho_i;
+        energy_i = Q.get(i,5);
+
+        rho_e    = Q.get(i,6);
+        u1_e     = Q.get(i,7)/rho_e;
+        u2_e     = Q.get(i,8)/rho_e;
+        u3_e     = Q.get(i,9)/rho_e;
+        energy_e = Q.get(i,10);
+
+        B1       = Q.get(i,11);
+        B2       = Q.get(i,12);
+        B3       = Q.get(i,13);
+        E1       = Q.get(i,14);
+        E2       = Q.get(i,15);
+        E3       = Q.get(i,16);
+
+        press_i  = (gamma-1.0e0)*(energy_i - 0.5e0*rho_i*(u1_i*u1_i 
+                    + u2_i*u2_i + u3_i*u3_i));
+
+        press_e  = (gamma-1.0e0)*(energy_e - 0.5e0*rho_e*(u1_e*u1_e
+                    + u2_e*u2_e + u3_e*u3_e));
+
+        // Flux function
+        flux.set(i,1,  rho_i*u1_i );
+        flux.set(i,2,  rho_i*u1_i*u1_i + press_i );
+        flux.set(i,3,  rho_i*u1_i*u2_i );
+        flux.set(i,4,  rho_i*u1_i*u3_i );
+        flux.set(i,5,  u1_i*(energy_i+press_i) ); 
+
+        flux.set(i,6,  rho_e*u1_e );
+        flux.set(i,7,  rho_e*u1_e*u1_e + press_e );
+        flux.set(i,8,  rho_e*u1_e*u2_e );
+        flux.set(i,9,  rho_e*u1_e*u3_e );
+        flux.set(i,10, u1_e*(energy_e+press_e) );
+
+        flux.set(i,11,  0.0 );
+        flux.set(i,12, -E3 );
+        flux.set(i,13,  E2 );
+        flux.set(i,14,  0.0 );
+        flux.set(i,15,  cs_light*cs_light*B3 );
+        flux.set(i,16, -cs_light*cs_light*B2 );
+    }
+
 
 }
 
--- apps/1d/two-fluid/grad-five-moment/lib/SourceTermFunc.cpp
+++ apps/1d/two-fluid/grad-five-moment/lib/SourceTermFunc.cpp
@@ -37,6 +37,11 @@ void SourceTermFunc(const dTensor1& xpts,
     const double debye_length   = global_ini_params.get_debye_length();
     const double larmor_radius  = global_ini_params.get_larmor_radius();
 
+    //const double mass_ratio     = global_ini_params.get_mass_ratio  ();
+    const double debye = global_ini_params.get_debye_length();
+    //const double larmor_radius  = global_ini_params.get_larmor_radius();
+
+
     // Derived parameters
     const double ion_mass       = 1.0;
     const double elc_mass       = ion_mass/mass_ratio;
@@ -54,55 +59,91 @@ void SourceTermFunc(const dTensor1& xpts,
         double x = xpts.get(i);
 
         // Variables
-        const double& rho_i    = qvals.get(i,_rho_i);
-        const double& M1_i     = qvals.get(i,_M1_i );
-        const double& M2_i     = qvals.get(i,_M2_i );
-        const double& M3_i     = qvals.get(i,_M3_i );
-        
-        const double& rho_e    = qvals.get(i,_rho_e);
-        const double& M1_e     = qvals.get(i,_M1_e );
-        const double& M2_e     = qvals.get(i,_M2_e );
-        const double& M3_e     = qvals.get(i,_M3_e );
-        
-        const double& B1       = qvals.get(i,_B1   );
-        const double& B2       = qvals.get(i,_B2   );
-        const double& B3       = qvals.get(i,_B3   );
-        const double& E1       = qvals.get(i,_E1   );
-        const double& E2       = qvals.get(i,_E2   );
-        const double& E3       = qvals.get(i,_E3   );
+//      const double& rho_i    = qvals.get(i,_rho_i);
+//      const double& M1_i     = qvals.get(i,_M1_i );
+//      const double& M2_i     = qvals.get(i,_M2_i );
+//      const double& M3_i     = qvals.get(i,_M3_i );
+//      
+//      const double& rho_e    = qvals.get(i,_rho_e);
+//      const double& M1_e     = qvals.get(i,_M1_e );
+//      const double& M2_e     = qvals.get(i,_M2_e );
+//      const double& M3_e     = qvals.get(i,_M3_e );
+//      
+//      const double& B1       = qvals.get(i,_B1   );
+//      const double& B2       = qvals.get(i,_B2   );
+//      const double& B3       = qvals.get(i,_B3   );
+//      const double& E1       = qvals.get(i,_E1   );
+//      const double& E2       = qvals.get(i,_E2   );
+//      const double& E3       = qvals.get(i,_E3   );
 
-        const double ion_charge_dens =  rho_i;
-        const double elc_charge_dens = -rho_e/mass_ratio;
-        const double charge_density  = ion_charge_dens + elc_charge_dens;
-        const double J1_i =  M1_i/ion_mass;
-        const double J2_i =  M2_i/ion_mass;
-        const double J3_i =  M3_i/ion_mass;
-        const double J1_e = -M1_e/elc_mass;
-        const double J2_e = -M2_e/elc_mass;
-        const double J3_e = -M3_e/elc_mass;
-        const double J1   = J1_i + J1_e;
-        const double J2   = J2_i + J2_e;
-        const double J3   = J3_i + J3_e;
-        
+//      const double ion_charge_dens =  rho_i;
+//      const double J1_i =  M1_i/ion_mass;
+//      const double J2_i =  M2_i/ion_mass;
+//      const double J3_i =  M3_i/ion_mass;
+//      const double J1_e = -M1_e/elc_mass;
+//      const double J2_e = -M2_e/elc_mass;
+//      const double J3_e = -M3_e/elc_mass;
+//      const double J1   = J1_i + J1_e;
+//      const double J2   = J2_i + J2_e;
+//      const double J3   = J3_i + J3_e;
+//      
+//      // Source term
+//      source.set(i,_rho_i,   0.0 );
+//      source.set(i,_M1_i ,   ( rho_i*E1 + (J2_i*B3 - J3_i*B2) ) / larmor_radius );
+//      source.set(i,_M2_i ,   ( rho_i*E2 + (J3_i*B1 - J1_i*B3) ) / larmor_radius );
+//      source.set(i,_M3_i ,   ( rho_i*E3 + (J1_i*B2 - J2_i*B1) ) / larmor_radius );
+//      source.set(i,_N_i  ,   (  J1_i*E1 +  J2_i*E2 + J3_i*E3  ) / larmor_radius );
+
+//      source.set(i,_rho_e,   0.0 );
+//      source.set(i,_M1_e ,   -rho_e*(E1 + (J2_e*B3 - J3_e*B2) )*mass_ratio / larmor_radius );
+//      source.set(i,_M2_e ,   -rho_e*(E2 + (J3_e*B1 - J1_e*B3) )*mass_ratio / larmor_radius );
+//      source.set(i,_M3_e ,   -rho_e*(E3 + (J1_e*B2 - J2_e*B1) )*mass_ratio / larmor_radius );
+//      source.set(i,_N_e  ,   -rho_e*(J1_e*E1 + J2_e*E2 + J3_e*E3)*mass_ratio / larmor_radius );
+
+//      source.set(i,_B1   ,   0.0 );
+//      source.set(i,_B2   ,   0.0 );
+//      source.set(i,_B3   ,   0.0 );
+//      source.set(i,_E1   ,   -J1*one_over_epsilon);
+//      source.set(i,_E2   ,   -J2*one_over_epsilon);
+//      source.set(i,_E3   ,   -J3*one_over_epsilon);
+
+        // ---- This (below here) was copied directly from DogPack ---- //
+
+        // Variables
+        const double rho_i    = qvals.get(i,1);
+        const double u1_i     = qvals.get(i,2)/rho_i;
+        const double u2_i     = qvals.get(i,3)/rho_i;
+        const double u3_i     = qvals.get(i,4)/rho_i;
+
+        const double rho_e    = qvals.get(i,6);
+        const double u1_e     = qvals.get(i,7)/rho_e;
+        const double u2_e     = qvals.get(i,8)/rho_e;
+        const double u3_e     = qvals.get(i,9)/rho_e;
+
+        const double B1       = qvals.get(i,11);
+        const double B2       = qvals.get(i,12);
+        const double B3       = qvals.get(i,13);
+        const double E1       = qvals.get(i,14);
+        const double E2       = qvals.get(i,15);
+        const double E3       = qvals.get(i,16);
+
         // Source term
-        source.set(i,_rho_i,   0.0 );
-        source.set(i,_M1_i ,   ( rho_i*E1 + (J2_i*B3 - J3_i*B2) ) / larmor_radius );
-        source.set(i,_M2_i ,   ( rho_i*E2 + (J3_i*B1 - J1_i*B3) ) / larmor_radius );
-        source.set(i,_M3_i ,   ( rho_i*E3 + (J1_i*B2 - J2_i*B1) ) / larmor_radius );
-        source.set(i,_N_i  ,   (  J1_i*E1 +  J2_i*E2 + J3_i*E3  ) / larmor_radius );
-
-        source.set(i,_rho_e,   0.0 );
-        source.set(i,_M1_e ,   ( elc_charge_dens*E1 + (J2_e*B3 - J3_e*B2) ) / larmor_radius );
-        source.set(i,_M2_e ,   ( elc_charge_dens*E2 + (J3_e*B1 - J1_e*B3) ) / larmor_radius );
-        source.set(i,_M3_e ,   ( elc_charge_dens*E3 + (J1_e*B2 - J2_e*B1) ) / larmor_radius );
-        source.set(i,_N_e  ,   ( J1_e*E1 + J2_e*E2 + J3_e*E3              ) / larmor_radius );
-
-        source.set(i,_B1   ,   0.0 );
-        source.set(i,_B2   ,   0.0 );
-        source.set(i,_B3   ,   0.0 );
-        source.set(i,_E1   ,   -J1*one_over_epsilon);
-        source.set(i,_E2   ,   -J2*one_over_epsilon);
-        source.set(i,_E3   ,   -J3*one_over_epsilon);
+        source.set(i,1,   0.0 );
+        source.set(i,2,   rho_i*(E1 + u2_i*B3 - u3_i*B2)/larmor_radius );
+        source.set(i,3,   rho_i*(E2 + u3_i*B1 - u1_i*B3)/larmor_radius );
+        source.set(i,4,   rho_i*(E3 + u1_i*B2 - u2_i*B1)/larmor_radius );
+        source.set(i,5,   rho_i*(u1_i*E1 + u2_i*E2 + u3_i*E3)/larmor_radius );
+        source.set(i,6,   0.0 );
+        source.set(i,7,  -rho_e*(E1 + u2_e*B3 - u3_e*B2)*mass_ratio/larmor_radius );
+        source.set(i,8,  -rho_e*(E2 + u3_e*B1 - u1_e*B3)*mass_ratio/larmor_radius );
+        source.set(i,9,  -rho_e*(E3 + u1_e*B2 - u2_e*B1)*mass_ratio/larmor_radius );
+        source.set(i,10, -rho_e*(u1_e*E1 + u2_e*E2 + u3_e*E3)*mass_ratio/larmor_radius );
+        source.set(i,11,  0.0 );
+        source.set(i,12,  0.0 );
+        source.set(i,13,  0.0 );
+        source.set(i,14,  (mass_ratio*rho_e*u1_e-rho_i*u1_i)/(debye*debye*larmor_radius) );
+        source.set(i,15,  (mass_ratio*rho_e*u2_e-rho_i*u2_i)/(debye*debye*larmor_radius) );
+        source.set(i,16,  (mass_ratio*rho_e*u3_e-rho_i*u3_i)/(debye*debye*larmor_radius) );
 
 /*
  * TODO - reinclude this section! -DS 7/9/2015
