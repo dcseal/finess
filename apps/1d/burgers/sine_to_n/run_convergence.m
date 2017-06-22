5bd2a3b23604912dc9de2076ff82e9c1a13a5947
David Seal <seal@usna.edu>
2017-06-22 15:29:41 -0400

    updated signatures for folder numbers in 1D burgers run_convergence

--- apps/1d/burgers/sine_to_n/run_convergence.m
+++ apps/1d/burgers/sine_to_n/run_convergence.m
@@ -1,9 +1,10 @@
 % Single script to call all the output folders and perform a convergence
 % study.
 n = 0;
-outputdir = ['output_', num2str( n, '%04d' ), '/parameters.ini' ];
+outputdir = ['output_', num2str( n, '%03d' ), '/parameters.ini' ];
+outputdir
 while( exist( outputdir, 'file') )
     plotfin1_nostop( outputdir );     % we think there's only one point ...
     n = n+1;
-    outputdir = ['output_', num2str( n, '%04d' ), '/parameters.ini' ];
+    outputdir = ['output_', num2str( n, '%03d' ), '/parameters.ini' ];
 end
