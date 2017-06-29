533eff21c7423c4ee23cad983e0c7f75dd8a896d
David Seal <seal@usna.edu>
2017-06-29 12:25:22 -0400

    adding a script to perform a handful of runs on order and cfl number for 2D Riemann problem

---
+++ apps/2d/euler/two_d_riemann_test/params_template.py
@@ -0,0 +1,91 @@
+
+# parameters template. 
+#
+# This should agree with the current parameters.ini with the exception of the
+# fields we wish to modify.
+finess_data_template = '''
+; Parameters common to FINESS applications
+[finess]
+ndims       = 2          ; 1, 2, or 3
+nout        = 1          ; number of output times to print results
+;tfinal      = 2.0        ; final time
+tfinal      = 0.80      ; final time (for RP1)
+initial_dt  = 1.0        ; initial dt
+max_dt      = 1.0e10     ; max allowable dt 
+max_cfl     = 1.00       ; max allowable Courant number
+desired_cfl = %(cfl)f    ; desired Courant number
+nv          = 500000     ; max number of time steps per call to DogSolve
+time_stepping_method = %(ts_method_str)s ; (e.g., Runge-Kutta, Lax-Wendroff, Multiderivative, User-Defined)
+space_order = %(s_order)i   ; order of accuracy in space
+time_order  = %(t_order)i   ; order of accuracy in time
+verbosity   = 1   ; verbosity of output
+mcapa       = 0   ; mcapa (capacity function index in aux arrays)
+maux        = 0   ; maux (number of aux arrays, maux >= mcapa)
+source_term = false   ; source term (1-yes, 0-no)
+meqn        = 5   ; number of equations
+output_dir  = %(output)s ; location of the output directory
+
+; -------------------------------------------------
+;   Cartesian grid data (ignored if Unstructured)
+; -------------------------------------------------
+[grid]
+mx    =  %(mx)i  ; number of grid elements in x-direction
+my    =  %(my)i  ; number of grid elements in y-direction
+mbc   =   11      ; number of ghost cells on each boundary
+
+;xlow  =  0.0e0  ; left end point
+;xhigh =  1.0e0  ; right end point
+;ylow  =  0.0e0  ; lower end point
+;yhigh =  1.0e0  ; upper end point
+
+; Grid spacing for RP1 and RP3
+xlow  = -0.5e0  ; left end point
+xhigh =  0.5e0  ; right end point
+ylow  = -0.5e0  ; lower end point
+yhigh =  0.5e0  ; upper end point
+
+; -------------------------------------------------
+;   WENO values
+; -------------------------------------------------
+[weno]
+weno_version  = JS     ; type of WENO reconstruction (e.g. JS, FD, Z)
+epsilon       = 1e-29  ; regulization parameter  ( epsilon > 0.0        )
+alpha_scaling = 1.1    ; scaling parameter       ( alpha_scaling >= 1.0 )
+
+; -------------------------------------------------
+;  Euler parameters
+; -------------------------------------------------
+[euler]
+gamma = 1.4                 ; gas constant
+riemann_problem_number = 1  ; Option [0,1,2,3,4,5] for which Riemann problem to solver.  
+'''
+
+qsub_template = '''
+#!/bin/bash -login
+#PBS -l walltime=00:24:00,nodes=1:ppn=%(proc_per_node)i
+#PBS -j oe
+#PBS -N run_2d_euler_%(descriptor)s
+ 
+# load necessary modules, e.g.
+# module load HDF5
+ 
+# change to the working directory where your code is located
+#
+# TODO - I don't know how to get this to use the environment variable $(FINESS)
+# that should be loaded with each login shell.  I tried doing the following:
+#
+# source ~/.bashrc
+#
+# but that didn't seem to work
+#
+# Instead, I am currently hard coding my own folder to this folder change:
+cd ~/code/finess/apps/2d/euler/two_d_riemann_test
+#cd $(PBS_O_WORKDIR)
+
+# Compile your code (uncomment this if you do not do this by hand)
+# make cleanallo; make -j;
+ 
+# call your executable
+pwd
+aprun -n1 ./finess.exe %(params_file_name)s 
+'''
---
+++ apps/2d/euler/two_d_riemann_test/run_qsub_samples.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+from contextlib import closing
+from subprocess import call, Popen, PIPE
+
+from params_template import finess_data_template, qsub_template
+
+def main(ts_method):
+    '''Create multiple qsub calls and run the code.'''
+
+    integrators   = ['Runge-Kutta', 'SDC', 'Lax-Wendroff', 'Multiderivative', 'User-Defined']
+    ts_method_str = integrators[ts_method]
+    print(ts_method_str)
+
+    cfl_range = [0.3,0.4]
+    sorders   = [5,7,9,11]
+    mx_range  = [64,128,256,512]
+
+    run_number = 0
+    for mx_now in mx_range:
+        for space_order in sorders:
+            for cfl in cfl_range:
+
+
+                mx_now = mx_now
+                my_now = mx_now
+
+                time_order = space_order
+                my_dictionary = {'s_order' : space_order, 't_order' : time_order,
+                        'cfl' : cfl,
+                        'mx' : mx_now, 'my' : my_now,
+                        'ts_method_str' : ts_method_str }
+                my_dictionary['output'] = 'output%(mx)03i_dt_%(s_order)02i_cfl%(cfl)2.1f' % my_dictionary
+
+                params_file_name = 'parameters%(mx)03i_dt_%(s_order)02i_cfl_%(cfl)2.1f.ini' % my_dictionary
+                with closing(open(params_file_name,'w')) as data:
+                    print >> data, finess_data_template% my_dictionary
+
+
+                my_dictionary['proc_per_node'] = 24
+                my_dictionary['descriptor'] = '%(mx)03i_dt_%(s_order)02i_cfl_%(cfl)2.1f' % my_dictionary
+                my_dictionary['params_file_name'] = params_file_name
+
+                qsub_file_name = 'run_app_%(mx)03i_dt_%(s_order)02i_cfl_%(cfl)2.1f.qsub' % my_dictionary
+                with closing(open(qsub_file_name,'w')) as data:
+                    print >> data, qsub_template % my_dictionary
+
+                # if you want to capture script output, do
+                #   Popen(thing to run, shell=True, stdout=PIPE).communicate()[0]
+#               cmd = 'qsub %s' % output_file_name
+#               print(cmd)
+        #       call(cmd, shell=True)
+
+                # run number
+                run_number = run_number + 1
+
+def parse_input( help_message ):
+    """Parse input arguments."""
+  
+    import argparse, sys
+  
+    parser = argparse.ArgumentParser (
+      prog = 'python '+ sys.argv[0],
+      description =    help_message,
+      formatter_class = argparse.RawTextHelpFormatter,
+    )
+
+    """
+    parser.add_argument('CFL',
+                      type = float,
+                      help = 'maximum Courant number in domain')
+ 
+    """
+
+    parser.add_argument('-s','--time_integrator',
+                      type    = int,
+                      choices = range(5),
+                      default =  4,
+                      dest    = 't_stepper',
+                      metavar = 'X',
+                      help    = 
+    '''choose integrator X for time-stepping:
+  0. Runge-Kutta
+  1. SDC
+  2. Lax-Wendroff
+  3. Multiderivative
+  4. 'User-Defined' time integrator.  (See Makefile for what gets linked to)
+(default: 4)''')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    """ Batch refinement study. 
+    
+    This script runs multiple samples, which can be later analyzed for a
+    convergence study."""
+
+    # Parse input arguments
+    args = parse_input( main.__doc__ )
+    print(args)
+    print('')
+
+    main( args.t_stepper )
