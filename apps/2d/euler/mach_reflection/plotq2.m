ecb96509eff9ea472f72a3e860f097dcd00f1236
David Seal <seal@usna.edu>
2017-07-20 12:01:21 -0400

    updated plotting routine for 2D Mach reflection problem

--- apps/2d/euler/mach_reflection/plotq2.m
+++ apps/2d/euler/mach_reflection/plotq2.m
@@ -17,9 +17,14 @@
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% gas constant
+% gas constant (this is a good example of how to pull items from the
+% parameters.ini file)
 global INI;
 gamma_gas = sscanf(INI.euler.gamma, '%e');
+
+% ---------------------------------------------------------------------------- %
+% Colorplot of the variable of choice
+% ---------------------------------------------------------------------------- %
 
 figure(1);
 clf;
@@ -38,34 +43,91 @@ t1 = title(['Density at t = ',num2str(time),'     [FINESS]']);
 set(t1,'fontsize',16);
 colorbar;
 caxis([1,25]);
-   
-   figure(2)
-   clf
-   plot(xc(:,1),qsoln(:,25,1),'b-');
 
-   % Grey scale figure plot that you see in a lot of papers.  For example, see 
-   % Figs 4.1 and 4.2 in 
-   %
-   %    "The Runge–Kutta Discontinuous Galerkin Method for Conservation
-   %     Laws V", Cockburn and Chi-Wang Shu, J. Comp. Phys., 141, 199–224 (1998).
-   %  
-   figure(3);
-   clf;
-   %contour(xc, yc, qsoln(:,:,m), linspace(1.3965, 22.682,30), '-k' );
-   % Woodward and Collela's contour lines:
-   contour(xc, yc, qsoln(:,:,m), linspace(1.728, 20.74,30), '-k' );
-   axis on; box on; grid off;
-   axis('equal');
-   axis([-0.05 3.25 -0.05 1.05]);
-   set(gca,'xtick',-4:0.5:4);
-   set(gca,'ytick',-4:0.5:4);
-   set(gca,'fontsize',16);
-   %t1 = title(['\rho(t,x,y) at t = ',num2str(time),'     [FINESS]']); 
-   %t1 = title(['Density at t = ',num2str(time),'     [FINESS]']); 
-   t1 = title(['Density']);
-   set(t1,'fontsize',16);
- 
+% ---------------------------------------------------------------------------- %
+% Schlieren plot for density (or any other variable)
+% ---------------------------------------------------------------------------- %
+
+% Grey scale figure plot that you see in a lot of papers.
+% figure('Position', [100, 100, 1049, 895]);
+figure(2);
+clf;
+
+rho=qsoln(:,:,1);
+
+%colors=linspace(1.0,3.0,10);
+h=xc(2,1)-xc(1,1);
+[px,py]=gradient(rho,h,h);
+v=sqrt(px.^2+py.^2)';
+
+fprintf('Smallest and largest value of the computed gradient = %f %f\n', max(max(v)), min(min(v)) );
+colormap( flipud(gray(2048)).^10 )
+hp = pcolor(xc,yc,v');
+set(hp, 'EdgeColor', 'none');
+
+% Fancier labels for the plots
+axis on; box on; grid off;
+axis('equal');
+axis([-0.05 3.25 -0.05 1.05]);
+set(gca,'xtick',-4:0.5:4);
+set(gca,'ytick',-4:0.5:4);
+set(gca,'fontsize',16);
+t1 = title(['Density at t = ',num2str(time),'     [FINESS]']); 
+set(t1,'fontsize',16);
+
+% ---------------------------------------------------------------------------- %
+% Grey scale topographical figure plot (this smooths out a lot of features but
+% seems to be the standard thing to plot)
+% ---------------------------------------------------------------------------- %
+
+% Grey scale figure plot that you see in a lot of papers.  For example, see 
+% Figs 4.1 and 4.2 in 
+%
+%    "The Runge–Kutta Discontinuous Galerkin Method for Conservation
+%     Laws V", Cockburn and Chi-Wang Shu, J. Comp. Phys., 141, 199–224 (1998).
+%  
+figure(3);
+clf;
+%contour(xc, yc, qsoln(:,:,m), linspace(1.3965, 22.682,30), '-k' );
+% Woodward and Collela's contour lines:
+contour(xc, yc, qsoln(:,:,m), linspace(1.728, 20.74,30), '-k' );
+axis on; box on; grid off;
+axis('equal');
+axis([-0.05 3.25 -0.05 1.05]);
+set(gca,'xtick',-4:0.5:4);
+set(gca,'ytick',-4:0.5:4);
+set(gca,'fontsize',16);
+%t1 = title(['\rho(t,x,y) at t = ',num2str(time),'     [FINESS]']); 
+%t1 = title(['Density at t = ',num2str(time),'     [FINESS]']); 
+t1 = title(['Density']);
+set(t1,'fontsize',16);
+
+% ---------------------------------------------------------------------------- %
+% Slice of the solution (to check for oscillations)
+% ---------------------------------------------------------------------------- %
+
+figure(4)
+clf
+plot( xc(:,1), qsoln(:,25,1), 'b-');
+
+set(gca,'xtick',-4:0.5:4);
+set(gca,'fontsize',16);
+xlabel('x');
+%ylabel('\rho', 'rot',0);
+ylabel('Density');
+set(gca,'plotboxaspectratio',[2 1 1]);
+
+axis([-0.05 3.25 -0.05 20.05]);
+
+t1 = title(['Density at y = ',num2str(xc(1,25)),'     [FINESS]']); 
+set(t1,'fontsize',16);
+
 figure(1)
+
+% ---------------------------------------------------------------------------- %
+% Plot the pretty pictures!
+% ---------------------------------------------------------------------------- %
+
 
 % n1 = frame number
 %fname = strcat( strcat( 'density', num2str(n1, '%02d' ) ), '.jpg' );
